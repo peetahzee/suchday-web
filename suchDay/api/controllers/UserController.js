26582f2ac401a152c56284f218fa1b4fb1234ed2
Peter Zhang <p@peetahzee.com>
2014-04-12 15:51:40 -0700

    specifying which user

--- suchDay/api/controllers/UserController.js
+++ suchDay/api/controllers/UserController.js
@@ -20,7 +20,8 @@ var googleapis = require('googleapis'),
     CLIENT_ID = '99312021964-5hc9j067l4svgh87sg3vc8ran4m1ctbm.apps.googleusercontent.com',
     CLIENT_SECRET = 'vAeqhqqdXQ7THNm8Y6zLWVm9',
     REDIRECT_URL = 'http://dash.ptzlabs.com/user/oAuthCallback',
-    atob = require('atob');
+    atob = require('atob'),
+    User = require('../models/User.js');
 
 var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
